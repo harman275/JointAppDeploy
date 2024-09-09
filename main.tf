def720b818d6bfa817e818f35f30794236e511a7
Mansukh Singh <you@example.com>
2024-09-09 19:49:23 +0530

    Hey New commit

---
+++ main.tf
@@ -0,0 +1,11 @@
+data "aws_key_pair" "existing_key" {
+  key_name = "testkey"
+}
+
+module "ec2" {
+  source        = "./modules/ec2"
+  ami           = var.ami # Replace with your preferred AMI
+  instance_type = var.instance_type
+  key_name  = data.aws_key_pair.existing_key.key_name
+  
+}
